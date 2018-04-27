657cf72ba0b3a3add110029cf2e5caec039b5231
Joan Almeida <joan.almeida@sondeos.com.ar>
2018-04-27 02:50:03 -0300

    Add dependencies and event handler

--- src/main/app/src/config.js
+++ src/main/app/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    "baseUrl": "http://localhost:8080/"
+    "baseUrl": "/"
 }
 
 export default config
---
+++ src/main/java/com/redbee/weatherbee/Services/LocationEvent.java
@@ -0,0 +1,4 @@
+package com.redbee.weatherbee.Services;
+
+public class LocationEvent {
+}
